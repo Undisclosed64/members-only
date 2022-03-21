f3548778003098cfb2cd58100f8185451d2be438
Undisclosed <undisclosed674@gmail.com>
2022-03-21 08:32:31 -0700

    add validation for confirming password

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
-const { body, validationResult } = require('express-validator');
+const { body, validationResult,check} = require('express-validator');
 
 
 
@@ -20,24 +20,35 @@ exports.getSignUp = function(req,res){
 
 //handle post request for signup
 exports.handleSignUp = [
+   
      // Validate and sanitize the field.
      body('name', 'User name is required').trim().isLength({ min: 1 }).escape(),
      body('password', 'Password can not be less than 4 characters').trim().isLength({ min: 4}).escape(),
 
+     //check that both password and confirm password field has the same value
+     check('password').exists(),
+     check(
+       'confirmPassword',
+       'password confirmation field must have the same value as the password field',
+     )
+       .exists()
+       .custom((value, { req }) => value === req.body.password),
+      
+       
+//process the request
     (req,res,next) => {
-
  const errors = validationResult(req);
 
   const user = new User({
       name:req.body.name,
-      password:req.body.password
+      password:req.body.password,
   })
- 
+  //check if the validation and santization has passed or not
   if (!errors.isEmpty()) {
-    
     res.render('signUpForm',{user:user,errors:errors.array()})
-
-  } else {
+  }
+   else {
+       //secure the password
     bcrypt.hash(user.password,10,(err,hashedPassword) => {
         if(err){
             return err;
@@ -47,6 +58,7 @@ exports.handleSignUp = [
       password:hashedPassword,
       _id:user._id
    })
+   //save the final user with secured password
             securedUser.save(function(err){
                 if(err){
                     return next(err);
