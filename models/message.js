4e6a63a2af674b6e8ff75e0dcd91fdb0f6f2e27d
Undisclosed <undisclosed674@gmail.com>
2022-03-23 00:39:15 -0700

    add schema for creating message

--- models/message.js
+++ models/message.js
@@ -1,1 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const MessageSchema = new Schema(
+  {
+   title: {type: String, required: true},
+   text:{type: String, required: true},
+   date:{type:Date,required:true}
+  })
+
+
+  MessageSchema.virtual('url')
+  .get(function(){
+return '/message/' + this._id
+  });
+
+module.exports = mongoose.model('Message',MessageSchema);
+    
 
