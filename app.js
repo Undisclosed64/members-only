2b2cc920e10c79bda1df7b285ff4ac324b0b51fa
Undisclosed <undisclosed674@gmail.com>
2022-03-25 03:12:46 -0700

    add changes

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const bcrypt = require('bcryptjs')
 
 //Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = process.env.MONGODB_URI;
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+//const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
