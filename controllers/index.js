065caea8f224cd8d072a6fffce3e88ae58c39d58
Undisclosed <undisclosed674@gmail.com>
2022-03-24 22:40:36 -0700

    return error msgs

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,6 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const Message = require('../models/message');
 const { body, validationResult,check} = require('express-validator');
-var async = require('async');
 
 
 //All the routes will go here
@@ -27,16 +26,14 @@ exports.getSignUp = function(req,res){
 exports.handleSignUp = [
    
      // Validate and sanitize the field.
-     body('username').trim().isLength({ min: 1 }).escape().withMessage('username is required') 
-     .isAlphanumeric().withMessage('Name has non-alphanumeric characters.'),
-
+     body('username').trim().isLength({ min: 1 }).escape().withMessage('Username is required'), 
      body('password', 'Password can not be less than 4 characters').trim().isLength({ min: 4}).escape(),
 
      //check that both password and confirm password field has the same value
      check('password').exists(),
      check(
        'confirmPassword',
-       'password confirmation field must have the same value as the password field',
+       'Password confirmation field must have the same value as the password field',
      )
        .exists()
        .custom((value, { req }) => value === req.body.password),
@@ -46,16 +43,17 @@ exports.handleSignUp = [
     (req,res,next) => {
  const errors = validationResult(req);
 
-  const user = new User({
-      name:req.body.username,
-      password:req.body.password,
-      isMember:false,
-  })
+ const user = new User({
+    username:req.body.username,
+    password:req.body.password,
+    isMember:false,
+})
+
   //check if the validation and santization has passed or not
   if (!errors.isEmpty()) {
-    res.render('signUpForm',{user:user,errors:errors.array()})
+    res.render('signUpForm',{errors:errors.array()})
   }
-   else {
+
        //secure the password
     bcrypt.hash(user.password,10,(err,hashedPassword) => {
         if(err){
@@ -77,19 +75,18 @@ exports.handleSignUp = [
         })
         
         })
-    }
-    }   
+    }  
 ]
-
-//handle get request for become-member route
-exports.becomeMemberGet = function(req,res){
-    res.render('becomeMember',{title:'Become a member',user:res.locals.currentUser});
-}
 
 //handle get login
 exports.getLogIn = function(req,res){
-    res.render('LogInForm',{title:'Log in'});
+    res.render('LogInForm');
 
+}
+
+//handle get request for become-member route
+exports.becomeMemberGet = function(req,res){
+    res.render('becomeMember',{user:res.locals.currentUser});
 }
       
 
@@ -97,7 +94,7 @@ exports.getLogIn = function(req,res){
 exports.becomeMemberPost = function(req,res,next){
 const passCode = req.body.passcode;
 if(passCode!== '432'){
-    res.send('Wrong passcode!')
+    res.render('becomeMember',{user:res.locals.currentUser,err:'Wrong passcode!'})
 } else {
     const user = new User(res.locals.currentUser);
     user.isMember = true;
@@ -110,36 +107,37 @@ if(passCode!== '432'){
 
 }
 
-exports.createMsgGet = function(req,res){
-if(res.locals.currentUser!==undefined){
-    res.render('createMsgForm',{title:'Create a message'})
-}
-res.redirect('/')
-}
+exports.createMsgGet = function(req,res,next){
+    if(res.locals.currentUser!==undefined){
+        res.render('createMsgForm',{user:req.user})
+    }
+    res.redirect('/log-in')
+};
 
-exports.createMsgPost = function(req,res,next){
-body('title').trim().isLength({ min: 1 }).escape().withMessage('Title is required!')
-body('text').trim().isLength({ min: 1 }).escape().withMessage('Text can not be empty!');
-body('date', 'Invalid date').optional({ checkFalsy: true }).isISO8601().toDate();
+exports.createMsgPost = [
 
-       
-//process the request
+body('title').trim().isLength({ min: 1 }).escape().withMessage('Title must not be empty!'),
+body('text').trim().isLength({ min: 1 }).escape().withMessage('Text can not be empty!'),
+
+(req,res,next) => {
+    //process the request
     const errors = validationResult(req);
-
      const message = new Message({
          title:req.body.title,
           text:req.body.text,
           date:Date.now(),
           user:req.user._id
      })
-     console.log(message.user)
      //check if the validation and santization has passed or not
      if (!errors.isEmpty()) {
-       res.render('createMsgForm',{message:message,errors:errors.array()})
+       res.render('createMsgForm',{errors:errors.array()})
      }
  message.save(function(err){
      if(err){
          return next(err)
-     }     res.redirect('/')
+     }  else {
+      res.redirect('/')
+     }
  })
 }
+]
